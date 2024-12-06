498e191005d0f07cb836a8c2a36d34484b5b4206
ARTHUR RODRIGUES <arthur.rodrigues14@fatec.sp.gov.br>
2024-12-05 22:38:01 -0300

    Alteracoes Arthur 05.12

---
+++ Models/TelaLogin.cs
@@ -0,0 +1,10 @@
+TelaLogin.css
+
+namespace findPet.Models
+{
+    public class TelaLoginModel
+    {
+        public required string Email { get; set; }
+        public required string Senha { get; set; }
+    }
+}

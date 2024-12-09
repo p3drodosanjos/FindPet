dd45c27a663f28d9163120d100d0f766f5e24690
Pedro Jasiel <pedrojasiel18@gmail.com>
2024-12-09 00:26:43 -0300

    Att Pedro

--- Controllers/TelaFeedController.cs
+++ Controllers/TelaFeedController.cs
@@ -20,6 +20,16 @@ namespace findPet.Controllers
 
             TelaPublicacaoFeed feed = new();
             return View(feed);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(TelaFeedModel model)
+        {
+            // Aqui você pode processar os dados do formulário, salvar no banco, etc.
+
+            // Redireciona para a TelaCadastro2 após o submit
+            return RedirectToAction("Index", "TelaPublicacao");
         }
 
     }
